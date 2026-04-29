43b9ff888120b26fa006293508b4b5a30115cede
v48939816-a11y <v48939816@gmail.com>
2026-04-29 22:24:27 +0300

    Add files via upload

---
+++ lang.js
@@ -0,0 +1,32 @@
+const i18n = {
+    en: {
+        bomb: "BOMB IT!",
+        stop: "STOP IT!",
+        sel_lang: "SELECT LANG",
+        save: "SAVE IT!",
+        back: "BACK",
+        ph_phone: "ENTER PHONE",
+        ph_proxy: "IP:PORT",
+        ph_auth: "USER:PASS"
+    },
+    ru: {
+        bomb: "ВЗОРВАТЬ!",
+        stop: "СТОП!",
+        sel_lang: "ВЫБРАТЬ ЯЗЫК",
+        save: "СОХРАНИТЬ!",
+        back: "НАЗАД",
+        ph_phone: "ВВЕДИТЕ НОМЕР",
+        ph_proxy: "IP:ПОРТ",
+        ph_auth: "ЛОГИН:ПАРОЛЬ"
+    },
+    ua: {
+        bomb: "ПІДІРВАТИ!",
+        stop: "СТОП!",
+        sel_lang: "ОБРАТИ МОВУ",
+        save: "ЗБЕРЕГТИ!",
+        back: "НАЗАД",
+        ph_phone: "ВВЕДІТЬ НОМЕР",
+        ph_proxy: "IP:ПОРТ",
+        ph_auth: "ЛОГІН:ПАРОЛЬ"
+    }
+};
